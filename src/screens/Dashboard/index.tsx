c41b347220a8191c8d62fefc70c8a922c4df91bb
rafal-hkek <rafal@hookooekoo.co>
2023-03-24 07:29:46 +0100

    feat: RIA-358 Api client

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {DashboardScreenProps} from './types';
 import {MainWrapper} from '@components/MainWrapper';
 import {StyledText} from '@components/typography/StyledText';
 import {useLogInNavigation} from '@src/navigation/hooks';
 import Screens from '@src/navigation/screens';
+import {apiClient} from '@api/apiClient';
+import {useGetUserProfile} from 'reinvest-app-common/src/services/queries/getProfile';
 
 export const Dashboard = ({}: DashboardScreenProps) => {
+  const {data, isLoading} = useGetUserProfile(apiClient);
+
+  useEffect(() => {
+    console.log({data, isLoading});
+  }, [data, isLoading]);
+
   const navigation = useLogInNavigation();
 
   return (
