6b63760d021173abf60f3e434e7e702a069bf411
Michal Gasiorek <michal@hookooekoo.co>
2023-03-27 13:28:13 +0200

    RIA-563: onboarding  Implement phone authentication screen

--- src/screens/Onboarding/flow-steps/index.ts
+++ src/screens/Onboarding/flow-steps/index.ts
@@ -1,3 +1,4 @@
+import {StepPhoneAuthentication} from './stepPhoneAuthentication';
 import {OnboardingFormFields} from '../types';
 import {createFormFlow} from 'reinvest-app-common/src/services/form-flow/index';
 import {StepFullName} from './stepFullName';
@@ -8,6 +9,7 @@ export const FLOW_STEPS = [
   StepAccountType,
   StepFullName,
   StepPhoneNumber,
+  StepPhoneAuthentication,
 ];
 
 export const onboardingFormFieldsInitialState: OnboardingFormFields = {
@@ -15,12 +17,8 @@ export const onboardingFormFieldsInitialState: OnboardingFormFields = {
   firstName: undefined,
   middleName: undefined,
   lastName: undefined,
-<<<<<<< HEAD
-=======
-  dateOfBirth: undefined,
   phoneNumber: undefined,
   _hasAuthenticatedPhoneNumber: false,
->>>>>>> 6042a3d (RIA-563: onboarding Implement phone number screen)
 };
 
 const [useOnboardingFormFlow, OnboardingFormFlowProvider] =
