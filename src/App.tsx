f79c4fadf70a49070b72a40bb4d719fec095bcc3
rafal-hkek <rafal@hookooekoo.co>
2023-03-30 18:43:25 +0200

    feat: RIA-360 Splash screen and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { RootNavigator } from '@navigation/RootNavigator';
 import { AuthProvider } from '@src/providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const queryClient = new QueryClient();
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
