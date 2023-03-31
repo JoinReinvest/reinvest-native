8abe55a06b9c483b17c5427b77bdf2ef0d3b5cb4
rafal-hkek <126668032+rafal-hkek@users.noreply.github.com>
2023-03-31 10:42:32 +0200

    feat: RIA-546 Sentry logger (#60)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { SENTRY_DNS } from '@env';
 import { RootNavigator } from '@navigation/RootNavigator';
+import * as Sentry from '@sentry/react-native';
 import { AuthProvider } from '@src/providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+
+/*
+ * Sending custom transaction for monitoring :
+ * const transaction = Sentry.startTransaction({ name: 'transaction type', tags: { startApp: 'Starting transaction' } });
+ * transaction.finish(Date.now());
+ */
+!__DEV__ &&
+  Sentry.init({
+    dsn: SENTRY_DNS,
+    tracesSampleRate: 1.0,
+  });
 
 const queryClient = new QueryClient();
 const App = () => {
@@ -19,4 +32,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
