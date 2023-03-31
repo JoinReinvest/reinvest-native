fea82afddfbadeacf94192ffdef656683e6839e1
rafal-hkek <rafal@hookooekoo.co>
2023-03-31 09:43:16 +0200

    feat: RIA-546 Sentry logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { SENTRY_DNS } from '@env';
 import { RootNavigator } from '@navigation/RootNavigator';
+import * as Sentry from '@sentry/react-native';
 import { AuthProvider } from '@src/providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
+
+/*
+ * Sending custom transaction for monitoring :
+ * const transaction = Sentry.startTransaction({ name: 'transaction type', tags: { startApp: 'Starting transaction' } });
+ * transaction.finish(Date.now());
+ */
+!__DEV__ &&
+  Sentry.init({
+    dsn: SENTRY_DNS,
+    tracesSampleRate: 1.0,
+  });
 
 const queryClient = new QueryClient();
 const App = () => {
@@ -14,4 +27,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
