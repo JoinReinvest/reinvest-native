b8c11ec3aee15c6467dd05756c5b73015fed7576
mgasiorek-hkek <127107414+mgasiorek-hkek@users.noreply.github.com>
2023-03-31 07:33:32 +0200

    RIA-589: Add loading screen when user is being fetched (#45)

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,11 +1,24 @@
-import { LogInNavigator } from '@navigation/LogInNavigator';
-import { LogOutNavigator } from '@navigation/LogOutNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import { useAuth } from '@src/providers/AuthProvider';
 import React from 'react';
+import { View } from 'react-native';
+
+import { Loader } from '../components/Loader';
+import { palette } from '../constants/theme';
+import { useAuth } from '../providers/AuthProvider';
+import { LogInNavigator } from './LogInNavigator';
+import { LogOutNavigator } from './LogOutNavigator';
+import { styles } from './styles';
 
 export const RootNavigator = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={[styles.loadingScreen]}>
+        <Loader color={palette.frostGreen} />
+      </View>
+    );
+  }
 
   return <NavigationContainer>{user ? <LogInNavigator /> : <LogOutNavigator />}</NavigationContainer>;
 };
---
+++ src/navigation/styles.ts
@@ -0,0 +1,11 @@
+import {palette} from '@src/constants/theme';
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  loadingScreen: {
+    flex: 1,
+    backgroundColor: palette.onboarding,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
