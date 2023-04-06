1487e103ddd5a5005f9c944aeccf107c30c80203
rafal-hkek <rafal@hookooekoo.co>
2023-04-06 15:26:32 +0200

    fix: RIA-722 Navigating inside invite code

--- src/screens/Invite/index.tsx
+++ src/screens/Invite/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Share } from 'react-native';
 import { useGetInvitationLink } from 'reinvest-app-common/src/services/queries/getInvitationLink';
 
@@ -7,45 +7,49 @@ import { Box } from '../../components/Containers/Box/Box';
 import { Input } from '../../components/Input';
 import { MainWrapper } from '../../components/MainWrapper';
 import { StyledText } from '../../components/typography/StyledText';
-import { isIOS } from '../../constants/common';
 import { palette } from '../../constants/theme';
 
 export const InviteScreen = () => {
-  const { data, isLoading } = useGetInvitationLink(apiClient);
+  const { data, isLoading, isSuccess } = useGetInvitationLink(apiClient);
+  const [url, setUrl] = useState<undefined | string>('');
 
   const share = async () => {
     await Share.share({
-      message: data?.url ?? '',
+      message: `${url}`,
     });
   };
 
-  const inputUrlValue = (isIOS ? data?.url?.substring(0, 37) : data?.url) ?? '';
+  useEffect(() => {
+    if (isSuccess) {
+      setUrl(data?.url || '');
+    }
+  }, [isSuccess]);
 
   return (
-    <MainWrapper isLoading={isLoading}>
+    <MainWrapper
+      isLoading={isLoading}
+      isScroll
+    >
       <Box fw>
         <Box my={'24'}>
           <StyledText variant={'h5'}>Invite friends and family to REINVEST more!</StyledText>
         </Box>
         <StyledText variant="h5">Earn up to $10 for every referral</StyledText>
-        {data && (
-          <>
-            <Box my={'16'}>{data && <StyledText variant="h4">Your Referral Code: {data?.url?.split('/').pop()}</StyledText>}</Box>
-            <Input
-              pointerEvents="none"
-              focusable={false}
-              caretHidden={true}
-              value={inputUrlValue}
-            />
-            <StyledText
-              onPress={share}
-              color={palette.dark3}
-              variant="paragraph"
-            >
-              copy and share the link above
-            </StyledText>
-          </>
-        )}
+        <>
+          <Box my={'16'}>{data && <StyledText variant="h4">Your Referral Code: {data?.url?.split('/').pop()}</StyledText>}</Box>
+          <Input
+            dataDetectorTypes={'link'}
+            value={`${url}`}
+            onChangeText={setUrl}
+          />
+          <StyledText
+            onPress={share}
+            color={palette.dark3}
+            variant="paragraph"
+          >
+            copy and share the link above
+          </StyledText>
+        </>
       </Box>
     </MainWrapper>
   );
