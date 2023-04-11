34ba3150d676d540626ee36253a226c0a0b8f79b
rafal-hkek <rafal@hookooekoo.co>
2023-04-11 13:39:21 +0200

    RIA-424 Search places input

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+
+type Timer = ReturnType<typeof setTimeout>;
+type ParamFunction = (...args: unknown[]) => void;
+
+export function useDebounce<Func extends ParamFunction>(func: Func, delay = 1000) {
+  const timer = useRef<Timer>();
+
+  useEffect(() => {
+    return () => {
+      if (!timer.current) return;
+
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const debouncedFunction = ((...args: unknown[]) => {
+    const newTimer = setTimeout(() => {
+      return func(...args);
+    }, delay);
+    clearTimeout(timer.current);
+    timer.current = newTimer;
+  }) as Func;
+
+  return debouncedFunction;
+}
--- src/screens/Onboarding/flow-steps/stepPermanentAddress.tsx
+++ src/screens/Onboarding/flow-steps/stepPermanentAddress.tsx
@@ -1,16 +1,21 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ScrollView, View } from 'react-native';
 import { STATES_AS_SELECT_OPTION } from 'reinvest-app-common/src/constants/states';
 import { allRequiredFieldsExists } from 'reinvest-app-common/src/services/form-flow';
 import { StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow/interfaces';
 import { DraftAccountType } from 'reinvest-app-common/src/types/graphql';
+import { Address } from 'reinvest-app-common/src/types/graphql';
 
 import { Button } from '../../../components/Button';
 import { FormTitle } from '../../../components/Forms/FormTitle';
+import { Icon } from '../../../components/Icon';
+import { SearchDialog } from '../../../components/Modals/ModalContent/PlacesModal';
 import { ProgressBar } from '../../../components/ProgressBar';
 import { Controller } from '../../../components/typography/Controller';
+import { palette } from '../../../constants/theme';
+import { useDialog } from '../../../providers/DialogProvider';
 import { formValidationRules } from '../../../utils/formValidationRules';
 import { Identifiers } from '../identifiers';
 import { OnboardingFormFields } from '../types';
@@ -20,6 +25,14 @@ import { styles } from './styles';
 type Fields = Exclude<OnboardingFormFields['permanentAddress'], undefined>;
 
 const schema = formValidationRules.address;
+
+const placeholders = {
+  addressLine1: 'Street Address or P.O. Box',
+  addressLine2: 'Apt, suite, unit, building, floor, etc',
+  zip: 'Zip Code',
+  state: 'State',
+  city: 'City',
+};
 
 export const StepPermanentAddress: StepParams<OnboardingFormFields> = {
   identifier: Identifiers.PERMANENT_ADDRESS,
@@ -50,8 +63,10 @@ export const StepPermanentAddress: StepParams<OnboardingFormFields> = {
     const initialValues: Fields = { addressLine1: '', addressLine2: '', city: '', state: '', zip: '', country: 'USA' };
     const defaultValues: Fields = storeFields.permanentAddress || initialValues;
 
+    const { openDialog } = useDialog();
+
     const { progressPercentage } = useOnboardingFormFlow();
-    const { handleSubmit, control, formState } = useForm<Fields>({
+    const { handleSubmit, control, formState, watch, reset } = useForm<Fields>({
       mode: 'all',
       resolver: zodResolver(schema),
       defaultValues,
@@ -65,48 +80,76 @@ export const StepPermanentAddress: StepParams<OnboardingFormFields> = {
       moveToNextStep();
     };
 
+    const addressWatched = watch('addressLine1');
+
+    const fillFieldsFromPrediction = (address: Address) => {
+      reset(address);
+    };
+
+    const showSearchDialog = useCallback(() => {
+      return openDialog(
+        <SearchDialog
+          value={addressWatched || ''}
+          placeholder={placeholders.addressLine1}
+          fillDetailsCallback={fillFieldsFromPrediction}
+        />,
+      );
+    }, [addressWatched]);
+
+    const getRightComponent = () => {
+      return (
+        <Icon
+          onPress={showSearchDialog}
+          color={palette.pureWhite}
+          icon="search"
+        />
+      );
+    };
+
     return (
       <>
         <View style={[styles.fw]}>
           <ProgressBar value={progressPercentage} />
         </View>
-        <ScrollView style={[styles.fw]}>
+        <ScrollView
+          style={[styles.fw]}
+          keyboardShouldPersistTaps="always"
+        >
           <FormTitle
             dark
             headline="What is your permanent address?"
             informationMessage="US Residents Only"
           />
-          {/* TODO: Change to Search Input when implemented */}
           <Controller
             onSubmit={handleSubmit(onSubmit)}
             control={control}
             fieldName="addressLine1"
-            inputProps={{ placeholder: 'Street Address or P.O. Box', dark: true }}
+            inputProps={{ placeholder: placeholders.addressLine1, dark: true, rightSection: getRightComponent() }}
           />
           <Controller
             onSubmit={handleSubmit(onSubmit)}
             control={control}
             fieldName="addressLine2"
-            inputProps={{ placeholder: 'Apt, suite, unit, building, floor, etc', dark: true }}
+            inputProps={{ placeholder: placeholders.addressLine2, dark: true }}
           />
           <Controller
             onSubmit={handleSubmit(onSubmit)}
             control={control}
             fieldName="city"
-            inputProps={{ placeholder: 'City', dark: true }}
+            inputProps={{ placeholder: placeholders.city, dark: true }}
           />
           <Controller
             type="dropdown"
             onSubmit={handleSubmit(onSubmit)}
             control={control}
             fieldName="state"
-            dropdownProps={{ placeholder: 'State', data: STATES_AS_SELECT_OPTION, dark: true }}
+            dropdownProps={{ placeholder: placeholders.state, data: STATES_AS_SELECT_OPTION, dark: true }}
           />
           <Controller
             onSubmit={handleSubmit(onSubmit)}
             control={control}
             fieldName="zip"
-            inputProps={{ placeholder: 'Zip Code', dark: true, maxLength: 5, keyboardType: 'numeric' }}
+            inputProps={{ placeholder: placeholders.zip, dark: true, maxLength: 5, keyboardType: 'numeric' }}
           />
         </ScrollView>
         <View
