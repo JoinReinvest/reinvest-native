caabb648c444ddc497432580b670b54042b95c78
rafal-hkek <126668032+rafal-hkek@users.noreply.github.com>
2023-04-12 10:55:10 +0200

    fix: RIA-759 Padding for card (#91)

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,13 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 import { palette } from '../../constants/theme';
-import { xScale, yScale } from '../../utils/scale';
+import { yScale } from '../../utils/scale';
 
 export const styles = StyleSheet.create({
   cardWrapper: {
     width: '100%',
     alignItems: 'center',
-    paddingVertical: yScale(24),
+    padding: yScale(24),
     borderWidth: 1,
     borderColor: palette.darkerGray,
   },
@@ -16,7 +16,6 @@ export const styles = StyleSheet.create({
   },
   cardContent: {
     textAlign: 'center',
-    maxWidth: xScale(209),
   },
   selected: {
     borderColor: palette.frostGreen,
