f2c3cf0bcb71806d5a01f352ee65290da0351ee8
rafal-hkek <rafal@hookooekoo.co>
2023-04-18 12:18:29 +0200

    RIA-812 Keyboard doesnt hide on signup signin

--- src/hooks/useKeyboardAware.ts
+++ src/hooks/useKeyboardAware.ts
@@ -6,10 +6,10 @@ import { AvoidSoftInput } from 'react-native-avoid-softinput';
  *There is no need to use scroll view with this implementation , use view for one input specific screens
  */
 
-export const useKeyboardAware = (enabled = true, offset?: number) => {
+export const useKeyboardAware = (enabled = true, offset?: number, shouldMimic = true) => {
   const onFocusEffect = useCallback(() => {
     if (enabled) {
-      AvoidSoftInput.setShouldMimicIOSBehavior(true);
+      AvoidSoftInput.setShouldMimicIOSBehavior(shouldMimic);
       AvoidSoftInput.setEnabled(true);
 
       if (offset) {
@@ -23,7 +23,7 @@ export const useKeyboardAware = (enabled = true, offset?: number) => {
     }
 
     return;
-  }, [enabled, offset]);
+  }, [shouldMimic, enabled, offset]);
 
   useFocusEffect(onFocusEffect);
 };
