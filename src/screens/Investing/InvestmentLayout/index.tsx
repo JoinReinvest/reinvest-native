848120cd3e218e7b72672ddc938b35b0c850cd9c
rafal-hkek <rafal@hookooekoo.co>
2023-05-09 15:15:57 +0200

    RIA-1020 Extend header with cancel investing option

--- src/screens/Investing/InvestmentLayout/index.tsx
+++ src/screens/Investing/InvestmentLayout/index.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useCallback, useLayoutEffect } from 'react';
 
 import { MainWrapper } from '../../../components/MainWrapper';
 import { TermsFooter } from '../../../components/TermsFooter';
+import { StyledText } from '../../../components/typography/StyledText';
 import { useStepBackOverride } from '../../../hooks/useBackOverride';
 import { useKeyboardAware } from '../../../hooks/useKeyboardAware';
 import { useLogInNavigation } from '../../../navigation/hooks';
 import { LogInStackParamList } from '../../../navigation/LogInNavigator/types';
 import { DialogProvider } from '../../../providers/DialogProvider';
 import { useInvestFlow } from '../flow-steps';
+import { Identifiers } from '../identifiers';
 import { InvestFormFields } from '../types';
 
 interface Props {
   shouldShowFooter?: boolean;
 }
+/*
+ * Add identifiers for skipping cancel element Probably will be Identifiers.PLAID , Identifiers.ACCOUNT_SELECTION
+ */
+
+const stepsWithCancelOption: Identifiers[] = [];
 export const InvestmentLayout = ({ shouldShowFooter = true }: Props) => {
-  const { CurrentStepView } = useInvestFlow();
+  const {
+    resetStoreFields,
+    CurrentStepView,
+    meta: { currentStepIdentifier },
+  } = useInvestFlow();
   const navigation = useLogInNavigation();
 
   useStepBackOverride<InvestFormFields, LogInStackParamList>(useInvestFlow, navigation, false);
   useKeyboardAware();
+
+  const onCancelInvestment = useCallback(async () => {
+    await resetStoreFields();
+    navigation.goBack();
+  }, [navigation, resetStoreFields]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: stepsWithCancelOption.includes(currentStepIdentifier as Identifiers)
+        ? undefined
+        : () => (
+            <StyledText
+              variant={'h6'}
+              onPress={onCancelInvestment}
+            >
+              Cancel
+            </StyledText>
+          ),
+    });
+  }, [currentStepIdentifier, navigation, onCancelInvestment]);
 
   return (
     <DialogProvider>
