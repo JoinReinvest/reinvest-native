89e53d07b3dac822f6e1fc75aff544af41fa4ddd
Michal Gasiorek <michal@hookooekoo.co>
2023-05-11 10:08:11 +0200

    RIA-1000: Implement notification screens

--- src/screens/Notifications/index.tsx
+++ src/screens/Notifications/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { MainWrapper } from '../../components/MainWrapper';
-import { StyledText } from '../../components/typography/StyledText';
+import { Notification } from '../../components/Notification';
+
+// TODO: Fetch notifications from API when query will be implemented
+
+const notifications = [
+  {
+    id: '0',
+    title: 'Dividend Update',
+    content: 'You earned $10 in dividends. Reinvest or withdraw your dividend.',
+    date: '2023-05-10',
+  },
+  {
+    id: '1',
+    title: 'Referral Reward',
+    content: 'You earned $10 for inviting friends and family. Reinvest or withdraw your reward.',
+    date: '2023-05-09',
+  },
+];
 
 export const Notifications = () => {
   const { top } = useSafeAreaInsets();
 
   return (
-    <MainWrapper style={{ paddingTop: top }}>
-      <StyledText variant="h6">Notifications screen</StyledText>
+    <MainWrapper style={{ paddingTop: top, paddingHorizontal: 0, rowGap: 8 }}>
+      {notifications.map(notification => (
+        <Notification
+          key={notification.id}
+          {...notification}
+          onPress={() => Alert.alert('Navigate to somewhere')}
+        />
+      ))}
     </MainWrapper>
   );
 };
