465c727920c503de65baa61ddbfeae3c9f6dbade
mgasiorek-hkek <127107414+mgasiorek-hkek@users.noreply.github.com>
2023-05-17 09:41:14 +0200

    RIA-1056: improve calendar to show next month (#185)

--- src/components/Calendar/utilities.ts
+++ src/components/Calendar/utilities.ts
@@ -1,46 +1,40 @@
 import dayjs from 'dayjs';
-import { formatDate } from 'reinvest-app-common/src/utilities/dates';
 
 import { Period } from './types';
 
-export const selectDatesForAutoSelection = (day: number, period: Period) => {
+const MAX_DAYS_IN_TWO_MONTHS = 62;
+
+export const getRecurringDates = (startingDate: dayjs.Dayjs, period: Period) => {
   const daysInMonth = dayjs().daysInMonth();
   const PERIOD_TO_DAYS = {
     WEEKLY: 7,
     'BI-WEEKLY': 14,
     MONTHLY: daysInMonth,
-    QUARTERLY: daysInMonth, // no need to calculate days count for quarterly period because it will not be shown for user in calendar
+    QUARTERLY: daysInMonth,
   };
 
   const autoSelectedDays = [];
+  const startingDateDay = +startingDate.format('D');
 
-  for (let i = day; i <= daysInMonth; i += PERIOD_TO_DAYS[period]) {
-    if (i === day) {
-      // do not auto select day that was already selected by the user
-      continue;
-    }
-
-    autoSelectedDays.push(i);
+  for (let i = PERIOD_TO_DAYS[period]; i < MAX_DAYS_IN_TWO_MONTHS - startingDateDay; i += PERIOD_TO_DAYS[period]) {
+    autoSelectedDays.push(startingDate.add(i, 'days'));
   }
 
   return autoSelectedDays;
 };
 
-export const getCalendarDays = () => {
-  const daysInMonth = [...new Array(dayjs().daysInMonth() + 1).keys()].slice(1); // start with 1 not 0
-  const firstWeekOffset = dayjs().startOf('month').day() === 0 ? 7 : dayjs().startOf('month').day(); // Sunday is indexed as 0 by default, changing it to 7 to fit calendar layout
+export const getCalendarDays = (date: dayjs.Dayjs) => {
+  const datesInMonth: dayjs.Dayjs[] = [];
+  const startOfMonthDate = dayjs(`${date.year()}-${(date.month() + 1).toString().padStart(2, '0')}-01`);
+
+  for (let i = 0; i < dayjs().daysInMonth(); i++) {
+    datesInMonth.push(startOfMonthDate.add(i, 'days'));
+  }
+
+  const daysInMonth = [...new Array(date.daysInMonth() + 1).keys()].map(day => dayjs(`${date.year()}-${date.month() + 1}-${day}`)).slice(1); // start with 1 not 0
+
+  const firstWeekOffset = date.startOf('month').day() === 0 ? 7 : date.startOf('month').day(); // Sunday is indexed as 0 by default, changing it to 7 to fit calendar layout
 
   // fill current month days with days from previous month to start current month at right day position
   return [...new Array(firstWeekOffset - 1), ...daysInMonth];
-};
-
-export const formatDayToDate = (...days: number[]) => {
-  const month = `${dayjs().month() + 1}`.padStart(2, '0');
-  const year = dayjs().year();
-
-  return days.map(day => {
-    const formattedDay = `${day}`.padStart(2, '0');
-
-    return formatDate(`${year}-${month}-${formattedDay}`, 'API', { currentFormat: 'DEFAULT' });
-  });
 };
