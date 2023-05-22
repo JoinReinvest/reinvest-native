bc1702e6dee86309d259285282877796b41d95d5
rafal-hkek <rafal@hookooekoo.co>
2023-05-22 14:34:21 +0200

    RIA-1109 Skipping investment

--- src/screens/Investing/flow-steps/recurringInvestmentInfo.tsx
+++ src/screens/Investing/flow-steps/recurringInvestmentInfo.tsx
@@ -6,21 +6,31 @@ import { Button } from '../../../components/Button';
 import { Box } from '../../../components/Containers/Box/Box';
 import { Icon } from '../../../components/Icon';
 import { StyledText } from '../../../components/typography/StyledText';
+import { useLogInNavigation } from '../../../navigation/hooks';
 import { Identifiers } from '../identifiers';
 import { InvestFormFields } from '../types';
+import { useInvestFlow } from './index';
 import { styles } from './styles';
 
 export const RecurringInvestmentInfo: StepParams<InvestFormFields> = {
   identifier: Identifiers.RECURRING_INVESTMENT,
   willBePartOfTheFlow: fields => !!fields._shouldDisplayRecurringInvestment,
 
-  Component: ({ moveToNextStep, updateStoreFields }: StepComponentProps<InvestFormFields>) => {
+  Component: ({ moveToNextStep, updateStoreFields, storeFields }: StepComponentProps<InvestFormFields>) => {
+    const { resetStoreFields } = useInvestFlow();
+    const { goBack } = useLogInNavigation();
     const handleContinue = async () => {
       await updateStoreFields({ isRecurringInvestment: true });
       moveToNextStep();
     };
 
     const handleSkip = async () => {
+      if (!storeFields.oneTimeInvestmentId) {
+        await resetStoreFields();
+
+        return goBack();
+      }
+
       await updateStoreFields({ isRecurringInvestment: false });
 
       moveToNextStep();
