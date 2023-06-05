376ace2e2abf54f4cb8f8c3b9e29a8d1ea11e4b8
Michal Gasiorek <michal@hookooekoo.co>
2023-06-05 13:00:23 +0200

    RIA-1296: forbid strings with just empty spaces

--- src/utils/formValidationRules.ts
+++ src/utils/formValidationRules.ts
@@ -7,7 +7,7 @@ import { STATE_NAMES } from '../constants/states';
 
 const requiredError = 'This field is required';
 const phoneWithoutCallingCodeRegex = /^(\d){3}-(\d){3}-(\d{3}|\d{4})$/;
-const standardRequiredString = zod.string().min(1, requiredError);
+const standardRequiredString = zod.string().trim().min(1, requiredError);
 
 export const MAX_APPLICANTS_COUNT = 5;
 
@@ -15,22 +15,30 @@ export const formValidationRules = {
   ...commonFormValidationRules,
   address: zod.object({
     addressLine1: standardRequiredString,
-    addressLine2: zod.string().nullable(),
+    addressLine2: zod.string().trim().nullable(),
     city: standardRequiredString,
     state: zod.enum(STATE_NAMES),
-    // eslint-disable-next-line security/detect-unsafe-regex
-    zip: zod.string().regex(/^\d{5}(?:-\d{4})?$/, { message: 'Invalid zip code' }),
+    zip: zod
+      .string()
+      .trim()
+      // eslint-disable-next-line security/detect-unsafe-regex
+      .regex(/^\d{5}(?:-\d{4})?$/, { message: 'Invalid zip code' }),
   }),
   numberAuthenticationCode: zod.string({ required_error: requiredError }).regex(/^([A-Z0-9]){6}$/i, { message: 'Invalid number authentication code' }),
-  date: zod.string({ required_error: requiredError }).regex(/^(\d{2})\/(\d{2})\/(\d{4})$/, { message: 'Invalid date format' }),
-  phone: zod.string({ required_error: requiredError }).regex(phoneWithoutCallingCodeRegex, {
+  date: zod
+    .string({ required_error: requiredError })
+    .trim()
+    .regex(/^(\d{2})\/(\d{2})\/(\d{4})$/, { message: 'Invalid date format' }),
+  phone: zod.string({ required_error: requiredError }).trim().regex(phoneWithoutCallingCodeRegex, {
     message: 'Invalid phone number',
   }),
   socialSecurityNumber: zod
     .string()
+    .trim()
     .regex(/^((?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|(\*{3}-\*{2}-\*{2}(?!0{2})\d{2})$/, { message: 'Invalid Social Security Number' }),
   maskedSocialSecurityNumber: zod
     .string()
+    .trim()
     .regex(/^((?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|(\*{3}-\*{2}-\d{4})$/, { message: 'Invalid Social Security Number' }),
 };
 
