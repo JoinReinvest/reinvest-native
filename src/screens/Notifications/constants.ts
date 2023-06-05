1d258d19b5e15075fc3a7dabfab587aeaac0aad4
mgasiorek-hkek <127107414+mgasiorek-hkek@users.noreply.github.com>
2023-06-05 14:42:48 +0200

    RIA-1306: show arrow only on notifications that require action (#260)

---
+++ src/screens/Notifications/constants.ts
@@ -0,0 +1,3 @@
+import { NotificationType } from 'reinvest-app-common/src/types/graphql';
+
+export const ACTIONABLE_NOTIFICATIONS = [NotificationType.DividendReceived, NotificationType.RewardDividendReceived, NotificationType.InvestmentFailed];
