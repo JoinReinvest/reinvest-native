4295b492fb5c21487fb3b956d5e6ed592e1ddcc5
rafal-hkek <rafal@hookooekoo.co>
2023-06-06 13:24:01 +0200

    RIA-1278 Bank account removed dialog

--- src/screens/ManageAccount/Screens/ManageBankAccount.tsx
+++ src/screens/ManageAccount/Screens/ManageBankAccount.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Alert } from 'react-native';
+import React, { useRef } from 'react';
 import { useReadBankAccount } from 'reinvest-app-common/src/services/queries/readBankAccount';
 
 import { getApiClient } from '../../../api/getApiClient';
@@ -7,23 +6,51 @@ import { Button } from '../../../components/Button';
 import { Box } from '../../../components/Containers/Box/Box';
 import { Loader } from '../../../components/Loader';
 import { MainWrapper } from '../../../components/MainWrapper';
+import { AccountUpdateConfirmation } from '../../../components/Modals/ModalContent/AccountUpdateConfirmation';
+import { HeaderWithLogo } from '../../../components/Modals/ModalHeaders/HeaderWithLogo';
 import { StyledText } from '../../../components/typography/StyledText';
 import { useCurrentAccount } from '../../../hooks/useActiveAccount';
 import { useLogInNavigation } from '../../../navigation/hooks';
 import Screens from '../../../navigation/screens';
+import { useDialog } from '../../../providers/DialogProvider';
 
 export const ManageBankAccount = () => {
   const { activeAccount } = useCurrentAccount();
   const { navigate } = useLogInNavigation();
+  const { openDialog } = useDialog();
+  const { goBack } = useLogInNavigation();
 
   const { data, isLoading } = useReadBankAccount(getApiClient, { accountId: activeAccount.id || '' });
-
+  const removedAccountRef = useRef(data?.accountNumber || '');
   const onPress = async () => {
-    navigate(Screens.BankAccount, { accountId: activeAccount.id || '', isUpdatingAccount: true });
+    navigate(Screens.BankAccount, { accountId: activeAccount.id || '', isUpdatingAccount: !!data?.accountNumber });
+  };
+
+  const showAccountRemovedDialog = () => {
+    openDialog(
+      <AccountUpdateConfirmation
+        accountNumber={removedAccountRef.current}
+        disclaimer="You will need to add an account to make future investments"
+      />,
+      {
+        showLogo: true,
+        header: (
+          <HeaderWithLogo
+            onClose={() => {
+              goBack();
+            }}
+          />
+        ),
+        closeIcon: false,
+      },
+    );
   };
 
   const onRemove = async () => {
-    Alert.alert('Removing bank account');
+    removedAccountRef.current = data?.accountNumber || '';
+    setTimeout(() => {
+      showAccountRemovedDialog();
+    }, 1000);
   };
 
   return (
@@ -48,6 +75,7 @@ export const ManageBankAccount = () => {
 
       <Button onPress={onPress}>Change</Button>
       <Button
+        disabled={!data}
         isDestructive
         variant="outlined"
         onPress={onRemove}
