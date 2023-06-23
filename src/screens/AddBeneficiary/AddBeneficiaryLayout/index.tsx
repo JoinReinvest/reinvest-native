3aac46ffc2c366cba13c694a3d0396094223d0dc
Michal Gasiorek <michal@hookooekoo.co>
2023-06-23 09:30:12 +0200

    RIA-1110: add close icon to investing promt after adding beneficiary

--- src/screens/AddBeneficiary/AddBeneficiaryLayout/index.tsx
+++ src/screens/AddBeneficiary/AddBeneficiaryLayout/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { Icon } from '../../../components/Icon';
 import { MainWrapper } from '../../../components/MainWrapper';
 import { TermsFooter } from '../../../components/TermsFooter';
+import { palette } from '../../../constants/theme';
 import { useStepBackOverride } from '../../../hooks/useBackOverride';
 import { useKeyboardAware } from '../../../hooks/useKeyboardAware';
 import { useLogInNavigation } from '../../../navigation/hooks';
 import { LogInStackParamList } from '../../../navigation/LogInNavigator/types';
+import Screens from '../../../navigation/screens';
 import { DialogProvider } from '../../../providers/DialogProvider';
 import { BeneficiaryCreationFormFields } from '../form-fields';
-import { Identifiers } from '../identifiers';
 import { useBeneficiaryCreationFlow } from '../steps';
 
 interface Props {
@@ -17,16 +19,27 @@ interface Props {
 export const AddBeneficiaryLayout = ({ shouldShowFooter = true }: Props) => {
   const {
     CurrentStepView,
-    meta: { currentStepIdentifier },
+    meta: { isLastStep },
   } = useBeneficiaryCreationFlow();
   const navigation = useLogInNavigation();
-  useStepBackOverride<BeneficiaryCreationFormFields, LogInStackParamList>(
-    useBeneficiaryCreationFlow,
-    navigation,
-    false,
-    currentStepIdentifier === Identifiers.INVESTING_PROMPT,
-  );
+  useStepBackOverride<BeneficiaryCreationFormFields, LogInStackParamList>(useBeneficiaryCreationFlow, navigation, true, isLastStep);
   useKeyboardAware();
+
+  useEffect(() => {
+    if (!isLastStep) {
+      return;
+    }
+
+    navigation.setOptions({
+      headerLeft: () => (
+        <Icon
+          icon="hamburgerClose"
+          color={palette.pureBlack}
+          onPress={() => navigation.replace(Screens.BottomNavigator, { screen: Screens.Dashboard })}
+        />
+      ),
+    });
+  }, [isLastStep, navigation]);
 
   return (
     <DialogProvider>
