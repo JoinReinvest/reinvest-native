49491ca15e4b6dd583f21867a652b5a1686df227
mgasiorek-hkek <127107414+mgasiorek-hkek@users.noreply.github.com>
2023-07-05 08:49:06 +0200

    hotfix: fix employment details not showing on the manage account screen (#345)

--- src/screens/ManageAccount/Screens/UpdateEmploymentDetails/index.tsx
+++ src/screens/ManageAccount/Screens/UpdateEmploymentDetails/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { useGetIndividualAccount } from 'reinvest-app-common/src/services/queries/getIndividualAccount';
 
-import { getApiClient } from '../../../../api/getApiClient';
 import { UpdateEmploymentDetailsFlowProvider } from './steps';
 import { UpdateEmploymentDetailsLayout } from './UpdateEmploymentDetailsLayout';
 
 export const UpdateEmploymentDetails = () => {
-  const { data: individualAccount } = useGetIndividualAccount(getApiClient);
-
   return (
-    <UpdateEmploymentDetailsFlowProvider
-      initialStoreFields={{
-        employmentStatus: individualAccount?.details?.employmentStatus?.status ?? undefined,
-        employer: individualAccount?.details?.employer ?? undefined,
-      }}
-    >
+    <UpdateEmploymentDetailsFlowProvider initialStoreFields={{}}>
       <UpdateEmploymentDetailsLayout />
     </UpdateEmploymentDetailsFlowProvider>
   );
