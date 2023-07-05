5240a2c99ed7748ab60877c62470a2ecd2d098c0
Michal Gasiorek <michal@hookooekoo.co>
2023-07-05 08:29:55 +0200

    hotfix: fix employment details not showing on the manage account screen

--- src/screens/ManageAccount/Screens/UpdateEmploymentDetails/steps/stepOriginalEmploymentDetails.tsx
+++ src/screens/ManageAccount/Screens/UpdateEmploymentDetails/steps/stepOriginalEmploymentDetails.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow';
-import { EmploymentStatus } from 'reinvest-app-common/src/types/graphql';
+import { useGetIndividualAccount } from 'reinvest-app-common/src/services/queries/getIndividualAccount';
+import { Employer, EmploymentStatus } from 'reinvest-app-common/src/types/graphql';
 
+import { getApiClient } from '../../../../../api/getApiClient';
 import { Button } from '../../../../../components/Button';
 import { Box } from '../../../../../components/Containers/Box/Box';
 import { Row } from '../../../../../components/Containers/Row';
+import { Loader } from '../../../../../components/Loader';
 import { StyledText } from '../../../../../components/typography/StyledText';
+import { palette } from '../../../../../constants/theme';
 import { UpdateEmploymentDetailsFormFields } from '../form-fields';
 import { Identifiers } from '../identifiers';
 
@@ -19,7 +23,23 @@ const EMPLOYMENT_STATUS_LABELS: { [key in EmploymentStatus]: string } = {
 export const StepOriginalEmploymentDetails: StepParams<UpdateEmploymentDetailsFormFields> = {
   identifier: Identifiers.ORIGINAL_EMPLOYMENT_DETAILS,
 
-  Component: ({ storeFields: { employmentStatus, employer }, moveToNextStep }: StepComponentProps<UpdateEmploymentDetailsFormFields>) => {
+  Component: ({ moveToNextStep }: StepComponentProps<UpdateEmploymentDetailsFormFields>) => {
+    const { data: individualAccount, isLoading } = useGetIndividualAccount(getApiClient);
+
+    const employmentStatus = individualAccount?.details?.employmentStatus?.status as EmploymentStatus | undefined;
+    const employer = individualAccount?.details?.employer as Employer | undefined;
+
+    if (!employmentStatus || !employer || isLoading) {
+      return (
+        <Box
+          fw
+          flex={1}
+        >
+          <Loader color={palette.pureBlack} />
+        </Box>
+      );
+    }
+
     const employerDetails = employer ? Object.values(employer) : [];
     const hasEmployer = employerDetails.some(value => !!value);
     const isEmployed = employmentStatus === EmploymentStatus.Employed;
