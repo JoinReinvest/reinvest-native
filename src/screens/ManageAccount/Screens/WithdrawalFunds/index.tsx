4236f77d446d93f8044f3489cd0bd7a7671cf4c0
reinny-hkek <reinny@hookooekoo.co>
2023-07-26 15:26:23 -0400

    feat: passing refetch interval to EVS chart, notification, and account stats queries

--- src/screens/ManageAccount/Screens/WithdrawalFunds/index.tsx
+++ src/screens/ManageAccount/Screens/WithdrawalFunds/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useGetAccountStats } from 'reinvest-app-common/src/services/queries/getAccountStats';
 
 import { getApiClient } from '../../../../api/getApiClient';
+import { QUERY_REFETCH_INTERVAL_MS } from '../../../../constants/queries';
 import { useCurrentAccount } from '../../../../hooks/useActiveAccount';
 import { WithdrawalFundsFormFields } from './form-fields';
 import { WithdrawalFundsFlowProvider } from './steps';
@@ -9,7 +10,10 @@ import { WithdrawalFundsLayout } from './WithdrawalFundsLayout';
 
 export const WithdrawalFunds = () => {
   const { activeAccount } = useCurrentAccount();
-  const { data: stats } = useGetAccountStats(getApiClient, { accountId: activeAccount.id ?? '', config: { enabled: !!activeAccount.id } });
+  const { data: stats } = useGetAccountStats(getApiClient, {
+    accountId: activeAccount.id ?? '',
+    config: { enabled: !!activeAccount.id, refetchInterval: QUERY_REFETCH_INTERVAL_MS },
+  });
 
   const initialStoreFields: WithdrawalFundsFormFields = {
     reason: '',
