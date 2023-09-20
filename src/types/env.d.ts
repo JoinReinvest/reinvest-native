cacbfc9cb71ecc5124aed85563fa94161ddcb2e1
msolecki <105009456+msolecki-hkek@users.noreply.github.com>
2023-09-20 14:35:13 +0200

    int update (#382)
    * remove unnecessary envs
    
    * feat: passing refetch interval to EVS chart, notification, and account stats queries (#381)
    
    ---------
    
    Co-authored-by: Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -6,11 +6,7 @@ declare module 'react-native-config' {
   export const AWS_REGION: string;
   export const REINVEST_SITE_URL: string;
   export const SENTRY_DNS: string;
-  export const CLOUDFRONT_INT: string;
-  export const CLOUDFRONT_DEV: string;
-  export const CLOUDFRONT_STG: string;
   export const GOOGLE_MAPS_API_KEY: string;
   export const GOOGLE_MAPS_PLACES_URL: string;
   export const GOOGLE_MAPS_AUTOCOMPLETE_URL: string;
-  export const FIREBASE_TOKEN: string;
 }
